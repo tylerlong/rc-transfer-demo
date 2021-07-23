3aca39ab374461c426f0c03b50df4e8721d28a79
Tyler Long <tyler4long@gmail.com>
2021-07-23 16:37:01 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import RingCentral from '@rc-ex/core';
 import waitFor from 'wait-for-async';
 import PubNubExtension from '@rc-ex/pubnub';
+import {ExtensionTelephonySessionsEvent} from '@rc-ex/core/lib/definitions';
 
 (async () => {
   const rc = new RingCentral({
@@ -19,8 +20,28 @@ import PubNubExtension from '@rc-ex/pubnub';
 
   await pubnubExtension.subscribe(
     ['/restapi/v1.0/account/~/extension/~/telephony/sessions'],
-    message => {
-      console.log(message);
+    async message => {
+      // console.log(JSON.stringify(message, null, 2));
+      const event = message as ExtensionTelephonySessionsEvent;
+      const parties = event.body?.parties?.filter(
+        p => p.status?.code === 'Answered'
+      );
+      if (parties && parties.length > 0) {
+        console.log('call answered');
+        await waitFor({interval: 1000});
+        const party = parties[0];
+        const r = await rc
+          .restapi()
+          .account()
+          .telephony()
+          .sessions(event.body?.telephonySessionId)
+          .parties(party.id)
+          .transfer()
+          .post({
+            extensionNumber: process.env.RINGCENTRAL_FORWARD_TO_EXTENSION,
+          });
+        console.log(r);
+      }
     }
   );
 
