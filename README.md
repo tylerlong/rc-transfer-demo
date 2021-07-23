81ac7a6958dbd3e7689c184e4994bf7a92a43fcc
Tyler Long <tyler4long@gmail.com>
2021-07-23 13:44:26 -0700

    Hello RC

---
+++ README.md
@@ -0,0 +1,3 @@
+# rc-transfer-demo
+
+This is a demo project to transfer a phone call using RingCentral API.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+import waitFor from 'wait-for-async';
+
+(async () => {
+  const rc = new RingCentral({
+    server: process.env.RINGCENTRAL_SERVER_URL,
+    clientId: process.env.RINGCENTRAL_CLIENT_ID,
+    clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+  });
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+  await waitFor({interval: 60000});
+  await rc.revoke();
+})();
