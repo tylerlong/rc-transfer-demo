276b0a40bafd53cc91b1ea3ae3665f28a2e93ca0
Tyler Long <tyler4long@gmail.com>
2021-07-23 16:08:35 -0700

    PubNub

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import RingCentral from '@rc-ex/core';
 import waitFor from 'wait-for-async';
+import PubNubExtension from '@rc-ex/pubnub';
 
 (async () => {
   const rc = new RingCentral({
@@ -7,11 +8,22 @@ import waitFor from 'wait-for-async';
     clientId: process.env.RINGCENTRAL_CLIENT_ID,
     clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
   });
+  const pubnubExtension = new PubNubExtension();
+  await rc.installExtension(pubnubExtension);
+
   await rc.authorize({
     username: process.env.RINGCENTRAL_USERNAME!,
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD!,
   });
+
+  await pubnubExtension.subscribe(
+    ['/restapi/v1.0/account/~/extension/~/telephony/sessions'],
+    message => {
+      console.log(message);
+    }
+  );
+
   await waitFor({interval: 60000});
   await rc.revoke();
 })();
